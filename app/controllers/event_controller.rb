208b960e0837e613e68c904601610eaf971ad60a
André Wendt <andrewen@AndreWendt.wan>
2008-06-12 12:59:10 +0200

    Completed event creation prototype

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,6 +1,12 @@
 class EventController < ApplicationController
 
   def new
+    @event = Event.new(params[:event])
+
+    return unless request.post?
+    if @event.save
+      redirect_to :action => 'show', :id => @event
+    end
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ActiveRecord::Base
 
   validates_presence_of :name, :date, :location
-
-  attr_accessor :name, :date, :location
+  validates_uniqueness_of :name
 
 end
