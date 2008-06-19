e59e22970be98e9bd6deddc1f58582708374c869
André Wendt <awendt@freeshell.org>
2008-06-19 18:36:50 +0200

    Added responses migration

---
+++ db/migrate/20080619163349_create_responses.rb
@@ -0,0 +1,18 @@
+class AddResponses < ActiveRecord::Migration
+  def self.up
+    create_table(:responses, :id => false) do |t|
+      t.references :event, :null => false
+      t.string :name,      :null => false, :limit => 255
+      t.boolean :rsvp,     :null => false
+
+      t.timestamps
+    end
+
+    add_index :responses, [:event_id, :name], :unique => true
+  end
+
+  def self.down
+    remove_index :responses, :column => [:event_id, :name]
+    drop_table :responses
+  end
+end
