dbd38b23fb787e049062693118644656b5e0646f
André Wendt <awendt@freeshell.org>
2008-06-28 14:05:53 +0200

    Changed response model validations * RSVP is not checked for presence anymore (does not work with false) * instead, validate for boolean values * model now also validates uniqueness of guest names for an event

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,9 @@
 class Response < ActiveRecord::Base
 
-  validates_presence_of :name, :rsvp, :event_id
+  validates_presence_of :name, :event_id
   validates_associated :event
+  validates_inclusion_of :rsvp, :in => [true, false]
+  validates_uniqueness_of :name, :scope => [:event_id]
 
   belongs_to :event
 
