a59e6e7af1d9d89599a25cf345748f7565ddcfbd
André Wendt <awendt@freeshell.org>
2008-06-29 10:37:40 +0200

    More specs on event controller

--- spec/controllers/event_controller_spec.rb
+++ spec/controllers/event_controller_spec.rb
@@ -4,7 +4,7 @@ describe EventController, "creating a new event" do
 
   before do
     @event = mock_model(Event, :permalink => "my-party")
-    Event.should_receive(:new).and_return(@event)
+    Event.stub!(:new).and_return(@event)
   end
 
   it "should redirect to response/post with a notice on successful save" do
@@ -14,11 +14,23 @@ describe EventController, "creating a new event" do
     response.should redirect_to(event_path(@event.permalink))
   end
 
-  it "should re-render the new template on failed save" do
+  it "should re-render the 'new' template on failed save" do
     @event.should_receive(:save).and_return(false)
     post :new
     flash[:notice].should be_nil
     response.should render_template('new')
+  end
+
+  it "should render the 'new' template on GET" do
+    get :new
+    flash[:notice].should be_nil
+    response.should render_template('new')
+  end
+
+  it "should pass the params to event" do
+    Event.should_receive(:new).with("name" => "My party", "date" => "now", "location" => "here").and_return(@event)
+    @event.stub!(:save)
+    post :new, :event => {:name => "My party", :date => "now", :location => "here"}
   end
 
 end
