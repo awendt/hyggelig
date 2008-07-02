3d2b0e20fbfc5845e06cfd41c7776d71a73b4485
André Wendt <awendt@freeshell.org>
2008-07-02 09:41:47 +0200

    Refactored EventController spec

--- spec/controllers/event_controller_spec.rb
+++ spec/controllers/event_controller_spec.rb
@@ -1,36 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+describe EventController, "processing GET requests" do
+
+  it "should render event/new on /" do
+    get :new
+    response.should render_template('new')
+    flash[:notice].should be_nil
+  end
+end
+
 describe EventController, "creating a new event" do
 
   before do
-    @event = mock_model(Event, :permalink => "my-party")
-    Event.stub!(:new).and_return(@event)
+    @options = { :name => "My Party", :date => "now", :location => "here" }
+  end
+
+  it "should save the event if valid" do
+    lambda { post :new, :event => @options }.should change(Event, :count).from(0).to(1)
   end
 
   it "should redirect to response/post with a notice on successful save" do
-    @event.should_receive(:save).and_return(true)
-    post :new
-    flash[:notice].should =~ /#{@event.permalink}/
-    response.should redirect_to(event_path(@event.permalink))
+    post :new, :event => @options
+    flash[:notice].should =~ /my-party/
+    response.should redirect_to(event_path("my-party"))
+  end
+
+  it "should not save the event if invalid" do
+    lambda { post :new, :event => @options.merge(:name => nil) }.should_not change(Event, :count)
   end
 
   it "should re-render the 'new' template on failed save" do
-    @event.should_receive(:save).and_return(false)
-    post :new
-    flash[:notice].should be_nil
-    response.should render_template('new')
-  end
-
-  it "should render the 'new' template on GET" do
-    get :new
+    post :new, :event => @options.merge(:name => nil)
     flash[:notice].should be_nil
     response.should render_template('new')
   end
 
   it "should pass the params to event" do
-    Event.should_receive(:new).with("name" => "My party", "date" => "now", "location" => "here").and_return(@event)
+    Event.should_receive(:new).with(@options.stringify_keys).and_return(@event)
     @event.stub!(:save)
-    post :new, :event => {:name => "My party", :date => "now", :location => "here"}
+    post :new, :event => @options
   end
 
 end
