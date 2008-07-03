8a3ccbea6a1f33fa185a4c7bc3ee9da6c1b74884
André Wendt <awendt@freeshell.org>
2008-07-03 09:37:43 +0200

    Added spec for unique guest names, refactored vars

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -3,11 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Response do
 
   before do
-    @options = { :name => "John Doe", :rsvp => true, :event_id => 1 } 
+    @attributes = { :name => "John Doe", :rsvp => true, :event_id => 1 } 
   end
 
   it "should be valid" do
-    Response.new(@options).should be_valid
+    Response.new(@attributes).should be_valid
   end
 
   it "should force presence of name" do
@@ -18,9 +18,18 @@ describe Response do
     Response.new.should have(1).error_on(:rsvp)
   end
 
-  it "should reject responses with invalid RSVPs" do
-    pending
-    Response.new(@options.merge(:rsvp => "yo")).should have(1).error_on(:rsvp)
+  it "should turn invalid RSVPs into false" do
+    Response.new(@attributes.merge(:rsvp => "yo")).rsvp.should be_false
+  end
+
+  it "should force uniqueness of guest names for one event" do
+    Response.create!(@attributes)
+    Response.new(@attributes).should have(1).error_on(:name)
+  end
+
+  it "should not force uniqueness of guest names across events" do
+    Response.create!(@attributes)
+    Response.new(@attributes.merge(:event_id => 2)).should have(0).errors_on(:name)
   end
 end
 
