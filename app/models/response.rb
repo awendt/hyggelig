1471d27fabce6911729d505fa1f3443209070daf
André Wendt <awendt@freeshell.org>
2008-08-28 22:37:05 +0200

    Now using the custom-err-msg plugin

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,7 +2,7 @@ class Response < ActiveRecord::Base
 
   validates_presence_of :name, :event_id
   validates_associated :event
-  validates_inclusion_of :rsvp, :in => [true, false]
+  validates_inclusion_of :rsvp, :in => [true, false], :message => "^#{l(:response, :confirm_or_decline)}"
   validates_uniqueness_of :name, :scope => [:event_id]
 
   belongs_to :event
