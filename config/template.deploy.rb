54ab72569ed1ce5a6848e9964f49b5d95085bf5f
André Wendt <awendt@freeshell.org>
2008-11-11 13:16:43 +0100

    Added deploy.rb template

---
+++ config/template.deploy.rb
@@ -0,0 +1,36 @@
+set :application, "hyggelig.org"
+set :repository,  "git://github.com/awendt/hyggelig.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+ssh_options[:compression] = false
+
+set :scm, "git"
+set :use_sudo, false
+
+# What you need to fill in
+set :user, "SSH login for the deployment user"
+set :deploy_to, "/path/to/deploy/target"
+set :port_number, "ask your hosting provider"
+role :app, "your app server"
+role :web, "your web server"
+role :db,  "your DB server", :primary => true
+
+namespace :deploy do
+
+  task :start, :roles => :app do
+    run "cd #{deploy_to}/current; mongrel_rails start -e production -p #{port_number} -d"
+  end
+  task :stop, :roles => :app do
+    run "cd #{deploy_to}/current; mongrel_rails stop"
+  end
+  task :restart, :roles => :app do
+    run "cd #{deploy_to}/current; mongrel_rails stop; mongrel_rails start -e production -p #{port_number} -d"
+  end
+
+  desc "Symlink the database config file from shared directory to current release dir"
+  task :symlink_database_yml do
+    run "ln -nsf #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+
+  after 'deploy:update_code', 'deploy:symlink_database_yml'
+end
