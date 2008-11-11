8b422a33571b66e24c2992f2c2fd8b7c21a83ba4
André Wendt <awendt@freeshell.org>
2008-11-11 18:02:37 +0100

    Spec for usage of h() on event page. Finally.

---
+++ spec/views/response/post_view_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "viewing an event and its responses" do
+
+  before do
+    @guest = mock_model(Response, :name => '<iframe src=""></iframe>', :attending? => true)
+    @event = mock_model(Event, :name => '<script></script>', :date => '<b>date</b>', :permalink => "script-script",
+      :location => '<iframe src=""></iframe>', :has_responses? => true, :valid? => true, 
+      :guests_by_reverse_chron => [@guest])
+    assigns[:event] = @event
+  end
+
+  it "should escape HTML in event name, date and location" do
+    render '/response/post'
+    response.should_not have_tag("script")
+    response.should_not have_tag("b")
+    response.should_not have_tag("iframe")
+  end
+end
