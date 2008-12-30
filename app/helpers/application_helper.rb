133be91d5f0281ccaff35088fc560c040ab0dd83
André Wendt <awendt@freeshell.org>
2008-12-30 13:09:16 +0100

    Errors on page creation appear as they did with Rails 2.0.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,29 @@ module ApplicationHelper
     end
   end
 
+  def short_error_message_for(*params)
+    options = params.extract_options!.symbolize_keys
+
+    if object = options.delete(:object)
+      objects = [object].flatten
+    else
+      objects = params.collect {|object_name| instance_variable_get("@#{object_name}") }.compact
+    end
+
+    count  = objects.inject(0) {|sum, object| sum + object.errors.count }
+    unless count.zero?
+      options[:object_name] ||= params.first
+
+      I18n.with_options :scope => [:activerecord, :errors, :template] do |locale|
+        object_name = options[:object_name].to_s.gsub('_', ' ')
+        object_name = I18n.t(object_name, :default => object_name, :scope => [:activerecord, :models], :count => 1)
+        header_message = locale.t :header, :count => count, :model => object_name
+
+        content_tag(:div, header_message, :id => 'errors')
+      end
+    else
+      ''
+    end
+  end
+
 end
