666eaf0e06d9bbb22982e411ee9ac30c6a92498f
André Wendt <awendt@freeshell.org>
2009-01-08 21:52:40 +0100

    Upgraded permalink_fu plugin

--- vendor/plugins/permalink_fu/lib/permalink_fu.rb
+++ vendor/plugins/permalink_fu/lib/permalink_fu.rb
@@ -9,27 +9,27 @@ module PermalinkFu
   class << self
     attr_accessor :translation_to
     attr_accessor :translation_from
+
+    # This method does the actual permalink escaping.
+    def escape(string)
+      result = ((translation_to && translation_from) ? Iconv.iconv(translation_to, translation_from, string) : string).to_s
+      result.gsub!(/[^\x00-\x7F]+/, '') # Remove anything non-ASCII entirely (e.g. diacritics).
+      result.gsub!(/[^\w_ \-]+/i,   '') # Remove unwanted chars.
+      result.gsub!(/[ \-]+/i,      '-') # No more than one of the separator in a row.
+      result.gsub!(/^\-|\-$/i,      '') # Remove leading/trailing separator.
+      result.downcase!
+      result.size.zero? ? random_permalink(string) : result
+    rescue
+      random_permalink(string)
+    end
     
-    def escape(str)
-      s = ((translation_to && translation_from) ? Iconv.iconv(translation_to, translation_from, str) : str).to_s
-      s.gsub!(/[^\w -]+/, '') # strip unwanted characters
-      s.strip!            # ohh la la
-      s.downcase!         #
-      s.gsub!(/[ -]+/, '-') # separate by single dashes
-      s
+    def random_permalink(seed = nil)
+      Digest::SHA1.hexdigest("#{seed}#{Time.now.to_s.split(//).sort_by {rand}}")
     end
   end
-  
-  def self.included(base)
-    base.extend ClassMethods
-    class << base
-      attr_accessor :permalink_options
-      attr_accessor :permalink_attributes
-      attr_accessor :permalink_field
-    end
-  end
-  
-  module ClassMethods
+
+  # This is the plugin method available on all ActiveRecord models.
+  module PluginMethods
     # Specifies the given field(s) as a permalink, meaning it is passed through PermalinkFu.escape and set to the permalink_field.  This
     # is done
     #
@@ -49,6 +49,9 @@ module PermalinkFu
     #
     #     # add a scope and specify the permalink field name
     #     has_permalink :title, :slug, :scope => :blog_id
+    #
+    #     # do not bother checking for a unique scope
+    #     has_permalink :title, :unique => false
     #   end
     #
     def has_permalink(attr_names = [], permalink_field = nil, options = {})
@@ -56,63 +59,119 @@ module PermalinkFu
         options = permalink_field
         permalink_field = nil
       end
-      self.permalink_attributes = Array(attr_names)
-      self.permalink_field      = (permalink_field || 'permalink').to_s
-      self.permalink_options    = options
-      before_validation :create_unique_permalink
-      evaluate_attribute_method permalink_field, "def #{self.permalink_field}=(new_value);write_attribute(:#{self.permalink_field}, PermalinkFu.escape(new_value));end", "#{self.permalink_field}="
-    end
-  end
-  
-protected
-  def create_unique_permalink
-    return unless should_create_permalink?
-    if send(self.class.permalink_field).to_s.empty?
-      send("#{self.class.permalink_field}=", create_permalink_for(self.class.permalink_attributes))
-    end
-    limit   = self.class.columns_hash[self.class.permalink_field].limit
-    base    = send("#{self.class.permalink_field}=", send(self.class.permalink_field)[0..limit - 1])
-    counter = 1
-    # oh how i wish i could use a hash for conditions
-    conditions = ["#{self.class.permalink_field} = ?", base]
-    unless new_record?
-      conditions.first << " and id != ?"
-      conditions       << id
-    end
-    if self.class.permalink_options[:scope]
-      conditions.first << " and #{self.class.permalink_options[:scope]} = ?"
-      conditions       << send(self.class.permalink_options[:scope])
-    end
-    while self.class.exists?(conditions)
-      suffix = "-#{counter += 1}"
-      conditions[1] = "#{base[0..limit-suffix.size-1]}#{suffix}"
-      send("#{self.class.permalink_field}=", conditions[1])
+      ClassMethods.setup_permalink_fu_on self do
+        self.permalink_attributes = Array(attr_names)
+        self.permalink_field      = (permalink_field || 'permalink').to_s
+        self.permalink_options    = {:unique => true}.update(options)
+      end
     end
   end
 
-  def create_permalink_for(attr_names)
-    attr_names.collect { |attr_name| send(attr_name).to_s } * " "
-  end
+  # Contains class methods for ActiveRecord models that have permalinks
+  module ClassMethods
+    def self.setup_permalink_fu_on(base)
+      base.extend self
+      class << base
+        attr_accessor :permalink_options
+        attr_accessor :permalink_attributes
+        attr_accessor :permalink_field
+      end
+      base.send :include, InstanceMethods
 
-private
-  def should_create_permalink?
-    if self.class.permalink_options[:if]
-      evaluate_method(self.class.permalink_options[:if])
-    elsif self.class.permalink_options[:unless]
-      !evaluate_method(self.class.permalink_options[:unless])
-    else
-      true
+      yield
+
+      if base.permalink_options[:unique]
+        base.before_validation :create_unique_permalink
+      else
+        base.before_validation :create_common_permalink
+      end
+      class << base
+        alias_method :define_attribute_methods_without_permalinks, :define_attribute_methods
+        alias_method :define_attribute_methods, :define_attribute_methods_with_permalinks
+      end
+    end
+
+    def define_attribute_methods_with_permalinks
+      if value = define_attribute_methods_without_permalinks
+        evaluate_attribute_method permalink_field, "def #{self.permalink_field}=(new_value);write_attribute(:#{self.permalink_field}, new_value ? PermalinkFu.escape(new_value) : nil);end", "#{self.permalink_field}="
+      end
+      value
     end
   end
 
-  def evaluate_method(method)
-    case method
-    when Symbol
-      send(method)
-    when String
-      eval(method, instance_eval { binding })
-    when Proc, Method
-      method.call(self)
+  # This contains instance methods for ActiveRecord models that have permalinks.
+  module InstanceMethods
+  protected
+    def create_common_permalink
+      return unless should_create_permalink?
+      if read_attribute(self.class.permalink_field).to_s.empty?
+        send("#{self.class.permalink_field}=", create_permalink_for(self.class.permalink_attributes))
+      end
+      limit   = self.class.columns_hash[self.class.permalink_field].limit
+      base    = send("#{self.class.permalink_field}=", read_attribute(self.class.permalink_field)[0..limit - 1])
+      [limit, base]
+    end
+
+    def create_unique_permalink
+      limit, base = create_common_permalink
+      return if limit.nil?
+      counter = 1
+      # oh how i wish i could use a hash for conditions
+      conditions = ["#{self.class.permalink_field} = ?", base]
+      unless new_record?
+        conditions.first << " and id != ?"
+        conditions       << id
+      end
+      if self.class.permalink_options[:scope]
+        [self.class.permalink_options[:scope]].flatten.each do |scope|
+          value = send(scope)
+          if value
+            conditions.first << " and #{scope} = ?"
+            conditions       << send(scope)
+          else
+            conditions.first << " and #{scope} IS NULL"
+          end
+        end
+      end
+      while self.class.exists?(conditions)
+        suffix = "-#{counter += 1}"
+        conditions[1] = "#{base[0..limit-suffix.size-1]}#{suffix}"
+        send("#{self.class.permalink_field}=", conditions[1])
+      end
+    end
+
+    def create_permalink_for(attr_names)
+      attr_names.collect { |attr_name| send(attr_name).to_s } * " "
+    end
+
+  private
+    def should_create_permalink?
+      return false unless permalink_fields_changed?
+      if self.class.permalink_options[:if]
+        evaluate_method(self.class.permalink_options[:if])
+      elsif self.class.permalink_options[:unless]
+        !evaluate_method(self.class.permalink_options[:unless])
+      else
+        true
+      end
+    end
+
+    def permalink_fields_changed?
+      self.class.permalink_attributes.any? do |attribute|
+        changed_method = "#{attribute}_changed?"
+        respond_to?(changed_method) ? send(changed_method) : true
+      end
+    end
+
+    def evaluate_method(method)
+      case method
+      when Symbol
+        send(method)
+      when String
+        eval(method, instance_eval { binding })
+      when Proc, Method
+        method.call(self)
+      end
     end
   end
 end
--- vendor/plugins/permalink_fu/test/permalink_fu_test.rb
+++ vendor/plugins/permalink_fu/test/permalink_fu_test.rb
@@ -1,6 +1,15 @@
 require 'test/unit'
 require File.join(File.dirname(__FILE__), '../lib/permalink_fu')
 
+begin
+  require 'rubygems'
+  require 'ruby-debug'
+  Debugger.start
+rescue LoadError
+  puts "no ruby debugger"
+end
+
+
 class FauxColumn < Struct.new(:limit)
 end
 
@@ -9,7 +18,11 @@ class BaseModel
     @columns_hash ||= {'permalink' => FauxColumn.new(100)}
   end
 
-  include PermalinkFu
+  def self.inherited(base)
+    subclasses << base
+  end
+
+  extend PermalinkFu::PluginMethods
   attr_accessor :id
   attr_accessor :title
   attr_accessor :extra
@@ -17,9 +30,10 @@ class BaseModel
   attr_accessor :foo
 
   class << self
-    attr_accessor :validation
+    attr_accessor :validation, :subclasses
   end
-  
+  self.subclasses = []
+
   def self.generated_methods
     @generated_methods ||= []
   end
@@ -31,7 +45,12 @@ class BaseModel
   def self.logger
     nil
   end
-  
+
+  def self.define_attribute_methods
+    return unless generated_methods.empty?
+    true
+  end
+
   # ripped from AR
   def self.evaluate_attribute_method(attr_name, method_definition, method_name=attr_name)
 
@@ -60,7 +79,7 @@ class BaseModel
   end
 
   def validate
-    send self.class.validation
+    send self.class.validation if self.class.validation
     permalink
   end
   
@@ -70,6 +89,10 @@ class BaseModel
   
   def write_attribute(key, value)
     instance_variable_set "@#{key}", value
+  end
+  
+  def read_attribute(key)
+    instance_variable_get "@#{key}"
   end
 end
 
@@ -86,6 +109,14 @@ class MockModel < BaseModel
   has_permalink :title
 end
 
+class CommonMockModel < BaseModel
+  def self.exists?(conditions)
+    false # oh noes
+  end
+
+  has_permalink :title, :unique => false
+end
+
 class ScopedModel < BaseModel
   def self.exists?(conditions)
     if conditions[1] == 'foo' && conditions[2] != 5
@@ -96,6 +127,32 @@ class ScopedModel < BaseModel
   end
 
   has_permalink :title, :scope => :foo
+end
+
+class ScopedModelForNilScope < BaseModel
+  def self.exists?(conditions)
+    (conditions[0] == 'permalink = ? and foo IS NULL') ? (conditions[1] == 'ack') : false
+  end
+
+  has_permalink :title, :scope => :foo
+end
+
+class OverrideModel < BaseModel
+  has_permalink :title
+  
+  def permalink
+    'not the permalink'
+  end
+end
+
+class ChangedModel < BaseModel
+  has_permalink :title  
+  def title_changed?; true; end
+end
+
+class NoChangeModel < BaseModel
+  has_permalink :title  
+  def title_changed?; false; end
 end
 
 class IfProcConditionModel < BaseModel
@@ -130,16 +187,19 @@ class MockModelExtra < BaseModel
   has_permalink [:title, :extra]
 end
 
+# trying to be like ActiveRecord, define the attribute methods manually
+BaseModel.subclasses.each { |c| c.send :define_attribute_methods }
+
 class PermalinkFuTest < Test::Unit::TestCase
   @@samples = {
-    'This IS a Tripped out title!!.!1  (well/ not really)' => 'this-is-a-tripped-out-title1-well-not-really',
-    '////// meph1sto r0x ! \\\\\\' => 'meph1sto-r0x',
-    'āčēģīķļņū' => 'acegiklnu',
-    '中文測試 chinese text' => 'chinese-text',
-    'fööbär' => 'foobar'
+    'This IS a Tripped out title!!.!1  (well/ not really)'.freeze => 'this-is-a-tripped-out-title1-well-not-really'.freeze,
+    '////// meph1sto r0x ! \\\\\\'.freeze => 'meph1sto-r0x'.freeze,
+    'āčēģīķļņū'.freeze => 'acegiklnu'.freeze,
+    '中文測試 chinese text'.freeze => 'chinese-text'.freeze,
+    'fööbär'.freeze => 'foobar'.freeze
   }
 
-  @@extra = { 'some-)()()-ExtRa!/// .data==?>    to \/\/test' => 'some-extra-data-to-test' }
+  @@extra = { 'some-)()()-ExtRa!/// .data==?>    to \/\/test'.freeze => 'some-extra-data-to-test'.freeze }
 
   def test_should_escape_permalinks
     @@samples.each do |from, to|
@@ -154,7 +214,15 @@ class PermalinkFuTest < Test::Unit::TestCase
       assert_equal to, @m.validate
     end
   end
-
+  
+  def test_should_escape_activerecord_model_with_existing_permalink
+    @m = MockModel.new
+    @@samples.each do |from, to|
+      @m.title = 'whatever'; @m.permalink = from
+      assert_equal to, @m.validate
+    end
+  end
+  
   def test_multiple_attribute_permalink
     @m = MockModelExtra.new
     @@samples.each do |from, to|
@@ -176,6 +244,13 @@ class PermalinkFuTest < Test::Unit::TestCase
     assert_equal 'bar-3', @m.permalink
   end
   
+  def test_should_common_permalink_if_unique_is_false
+    @m = CommonMockModel.new
+    @m.permalink = 'foo'
+    @m.validate
+    assert_equal 'foo', @m.permalink
+  end
+  
   def test_should_not_check_itself_for_unique_permalink
     @m = MockModel.new
     @m.id = 2
@@ -189,7 +264,7 @@ class PermalinkFuTest < Test::Unit::TestCase
     @m.permalink = 'foo'
     @m.validate
     assert_equal 'foo-2', @m.permalink
-
+  
     @m.foo = 5
     @m.permalink = 'foo'
     @m.validate
@@ -256,5 +331,53 @@ class PermalinkFuTest < Test::Unit::TestCase
     @m.title = 'make me a permalink'
     @m.validate
     assert_not_nil @m.permalink
+  end
+  
+  def test_should_allow_override_of_permalink_method
+    @m = OverrideModel.new
+    @m.write_attribute(:permalink, 'the permalink')
+    assert_not_equal @m.permalink, @m.read_attribute(:permalink)
+  end
+  
+  def test_should_create_permalink_from_attribute_not_attribute_accessor
+    @m = OverrideModel.new
+    @m.title = 'the permalink'
+    @m.validate
+    assert_equal 'the-permalink', @m.read_attribute(:permalink)
+  end
+  
+  def test_should_not_update_permalink_unless_field_changed
+    @m = NoChangeModel.new
+    @m.title = 'the permalink'
+    @m.validate
+    assert_nil @m.read_attribute(:permalink)
+  end
+  
+  def test_should_update_permalink_if_field_changed
+    @m = OverrideModel.new
+    @m.title = 'the permalink'
+    @m.validate
+    assert_equal 'the-permalink', @m.read_attribute(:permalink)
+  end
+  
+  def test_should_update_permalink_if_changed_method_does_not_exist
+    @m = OverrideModel.new
+    @m.title = 'the permalink'
+    @m.validate
+    assert_equal 'the-permalink', @m.read_attribute(:permalink)
+  end
+  
+  def test_should_work_correctly_for_scoped_fields_with_nil_value
+    s1 = ScopedModelForNilScope.new
+    s1.title = 'ack'
+    s1.foo = 3
+    s1.validate
+    assert_equal 'ack', s1.permalink
+    
+    s2 = ScopedModelForNilScope.new
+    s2.title = 'ack'
+    s2.foo = nil
+    s2.validate
+    assert_equal 'ack-2', s2.permalink
   end
 end
