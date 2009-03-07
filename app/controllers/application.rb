b3deb51b80c2fee11b46aa594d3b1b64d62d94eb
André Wendt <awendt@freeshell.org>
2009-03-07 13:32:29 +0100

    Refactoring of content negotiation into plugin

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -24,29 +24,13 @@ private
     else
       I18n.locale = lang_intersection.first
     end
+    RoutingFilter::Locale.default_locale = client_accepted_languages.first || :en
     true
   end
 
   def prefered_langs
     params[:locale].to_a.map(&:to_sym) + client_accepted_languages
   end
-
-  def client_accepted_languages
-    parse_http_accept_language_header(request.env["HTTP_ACCEPT_LANGUAGE"])
-  end
-
-  def parse_http_accept_language_header(header)
-    return [] unless header
-    # assumes the languages are ordered by quality value
-    # (see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4)
-    http_accept_langs = header.split(/,/)
-
-    # strip qvalues
-    http_accept_langs.collect! { |lang| lang.split(/;/).first.split(/-/).first }
-
-    http_accept_langs.uniq.map(&:to_sym)
-  end
-  memoize :parse_http_accept_language_header
 
   def available_locales
     Dir["config/locales/*.yml"].map { |file| File.basename(file, ".yml").to_sym }
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -9,27 +9,27 @@ describe ApplicationController, "before filter to set the locale" do
 
   it "should always return TRUE" do
     request = mock('request', :env => { "HTTP_ACCEPT_LANGUAGE" => "en-US"} )
-    controller.should_receive(:request).and_return(request)
+    controller.should_receive(:request).any_number_of_times.and_return(request)
     controller.send(:set_locale).should be_true
   end
 
   it "should set the localization language to English if header asks for English contents" do
     request = mock('request', :env => { "HTTP_ACCEPT_LANGUAGE" => "en-US"} )
-    controller.should_receive(:request).and_return(request)
+    controller.should_receive(:request).any_number_of_times.and_return(request)
     I18n.should_receive(:locale=).with(:en)
     controller.send(:set_locale).should be_true
   end
 
   it "should set the localization language to German if header asks for German contents" do
     request = mock('request', :env => { "HTTP_ACCEPT_LANGUAGE" => "de"} )
-    controller.should_receive(:request).and_return(request)
+    controller.should_receive(:request).any_number_of_times.and_return(request)
     I18n.should_receive(:locale=).with(:de)
     controller.send(:set_locale).should be_true
   end
 
   it "should set the localization language to English as default" do
     request = mock('request', :env => { "HTTP_ACCEPT_LANGUAGE" => "dv"} )
-    controller.should_receive(:request).and_return(request)
+    controller.should_receive(:request).any_number_of_times.and_return(request)
     I18n.should_receive(:locale=).with(:en)
     controller.send(:set_locale).should be_true
   end
