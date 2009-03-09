7044baf8a11672f32227bf29e6c60b4763890f7f
André Wendt <awendt@freeshell.org>
2009-03-09 22:31:45 +0100

    More validations on Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,12 @@
 class Event < ActiveRecord::Base
 
-  RESERVED_NAMES = %w(new event feed response faq faqs demo help).freeze
+  RESERVED_NAMES = %w(new event feed response faq faqs demo help tour).freeze
   validates_presence_of :name, :date, :location
   validates_each :name do |record, attr, value|
     record.errors.add attr, I18n.t(:'active_record_messages.exclusion') \
       if value && RESERVED_NAMES.include?(value.downcase)
   end
+  validates_length_of :name, :minimum => 3
 
   has_permalink :name, :permalink
   has_many :responses, :dependent => :delete_all
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -10,8 +10,8 @@ describe Event do
     Event.new(@attributes).should be_valid
   end
 
-  it "should force presence of name" do
-    Event.new.should have(1).error_on(:name)
+  it "should force presence of name and a minimum on its length" do
+    Event.new.should have(2).error_on(:name)
   end
 
   it "should force presence of date" do
@@ -20,6 +20,10 @@ describe Event do
 
   it "should force presence of location" do
     Event.new.should have(1).error_on(:location)
+  end
+
+  it "should force a minimum length on name" do
+    Event.new(@attributes.merge(:name => 'Pa')).should have(1).error_on(:name)
   end
 
   it "should not force uniqueness of name (permalink changes accordingly)" do
@@ -36,6 +40,7 @@ describe Event do
     e5 = Event.new(:name => "FAQ", :date => "date", :location => "loc")
     e6 = Event.new(:name => "Demo", :date => "date", :location => "loc")
     e7 = Event.new(:name => "HELP", :date => "date", :location => "loc")
+    e8 = Event.new(:name => "tOUr", :date => "date", :location => "loc")
     e1.should have(1).error_on(:name)
     e2.should have(1).error_on(:name)
     e3.should have(1).error_on(:name)
@@ -43,6 +48,7 @@ describe Event do
     e5.should have(1).error_on(:name)
     e6.should have(1).error_on(:name)
     e7.should have(1).error_on(:name)
+    e8.should have(1).error_on(:name)
   end
 
   it "should create a permalink along with the event" do
