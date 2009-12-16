5873e0a03f9b6ee9dc5bebde245dcb2fe77a6e99
André Wendt <awendt@freeshell.org>
2009-12-16 20:30:33 +0100

    Rails 2.3.5 and RSpec upgrade

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,3 +20,6 @@ config.action_controller.allow_forgery_protection    = false
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
+
+config.gem "rspec", :lib => 'spec', :version => '1.3.0'
+config.gem "rspec-rails", :lib => 'spec/rails', :version => '1.3.2'
