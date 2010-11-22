eab4b1db247cb0eaa4c0ecc6edf990e45216e35c
André Wendt <awendt@freeshell.org>
2010-11-22 09:05:41 +0100

    Upgrade to Rails 2.3.9, preparing for Rails 3

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '64b14d925b50329d9d1d48ea028dfdbf53153f14143bf88c9534b1fb50f28699958793368f2a8a87d0ca1c20390e0b9bb4ad2a18c6b2b0e31533b2af2528b2ea';
