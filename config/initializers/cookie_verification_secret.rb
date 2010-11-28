7487d0737e3b1846be32b03cfbb0c88495ecc99b
André Wendt <awendt@freeshell.org>
2010-11-28 22:57:41 +0100

    Fixed broken cookie verification
    Heroku doesn't allow filesystem access, file should be read _after_ ENV

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-secret_token = File.read("#{Rails.root}/config/session_key_secret")
-Rails.application.config.secret_token = ENV['SESSION_SECRET'] || "#{secret_token}"
+Rails.application.config.secret_token = ENV['SESSION_SECRET'] ||
+    File.read("#{Rails.root}/config/session_key_secret")
