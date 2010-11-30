03a6b328b50701823b03ded9e104fb2d6f2fe067
André Wendt <awendt@freeshell.org>
2010-11-30 22:01:19 +0100

    Fix: Event pages can be created again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,6 @@ private
 
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    params[:locale] ? { :locale => I18n.locale } : {}
+    params[:locale].blank? ? {:locale => nil} : {:locale => I18n.locale}
   end
 end
